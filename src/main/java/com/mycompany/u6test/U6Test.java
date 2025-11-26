c4ca80caf1df3cb20d19026b402795c83b95d361
PatrickVernon <pvernon@uxbridgecollege.ac.uk>
2025-11-26 17:48:34 +0000

    First Commit

---
+++ src/main/java/com/mycompany/u6test/U6Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u6test;
+
+/**
+ *
+ * @author pvernon
+ */
+public class U6Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
